7dffabd3b57760a90011801291ed0b2f370346b3
Brennan <bh230@duke.edu>
2023-10-03 16:33:11 -0400

    added data classes, modified index.py/.html to support new/updated classes

--- app/index.py
+++ app/index.py
@@ -3,7 +3,7 @@ from flask_login import current_user
 import datetime
 
 from .models.product import Product
-from .models.purchase import Purchase
+from .models.orderfact import OrderFact
 
 from flask import Blueprint
 bp = Blueprint('index', __name__)
@@ -12,14 +12,10 @@ bp = Blueprint('index', __name__)
 @bp.route('/')
 def index():
     # get all available products for sale:
-    products = Product.get_all(True)
+    products = Product.get_all_less_than_equal_to_price(100000000)
     # find the products current user has bought:
     if current_user.is_authenticated:
-        purchases = Purchase.get_all_by_uid_since(
-            current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
+        purchases = OrderFact.get_orders_given_buyer(current_user.id)
+        return render_template('index.html', avail_products=products, purchase_history=purchases)
     else:
-        purchases = None
-    # render the page by adding information to the index.html file
-    return render_template('index.html',
-                           avail_products=products,
-                           purchase_history=purchases)
+        return render_template('index.html', avail_products=products)
