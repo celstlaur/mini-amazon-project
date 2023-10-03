8077b608dab3e35793d088fdf281435f3c51443e
Brennan <bh230@duke.edu>
2023-10-03 11:51:42 -0400

    modified create / load sql to support db design, added small examples csv files

--- db/load.sql
+++ db/load.sql
@@ -10,7 +10,27 @@ SELECT pg_catalog.setval('public.products_id_seq',
                          (SELECT MAX(id)+1 FROM Products),
                          false);
 
-\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.purchases_id_seq',
-                         (SELECT MAX(id)+1 FROM Purchases),
+\COPY OrderFact FROM 'OrderFact.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.orderfact_id_seq',
+                         (SELECT MAX(id)+1 FROM OrderFact),
                          false);
+
+\COPY Sellers FROM 'Sellers.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY Balance FROM 'Balance.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY CartContents FROM 'CartContents.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY Fulfills FROM 'Fulfills.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY HasInventory FROM 'HasInventory.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY OrderContents FROM 'OrderContents.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY ReviewedProduct FROM 'ReviewedProduct.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY ReviewedSeller FROM 'ReviewedSeller.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY SavedForLaterContents FROM 'SavedForLaterContents.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY Search FROM 'Search.csv' WITH DELIMITER ',' NULL '' CSV
