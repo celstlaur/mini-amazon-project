2e536f75827f67673c356fd3c55aaf1ca7cffc65
Sierra <sas227@duke.edu>
2023-11-09 11:47:37 -0500

    add feedback is working

--- app/models/feedbackitem.py
+++ app/models/feedbackitem.py
@@ -1,4 +1,5 @@
 from flask import current_app as app
+import datetime
 
 class FeedbackItem:
     def __init__(self, reviewed, review, timestamp):
@@ -35,3 +36,20 @@ class FeedbackItem:
                             ORDER BY time_reviewed DESC LIMIT 5''', user_id = user_id)
         
         return [FeedbackItem(*row) for row in rows] if rows else None
+    
+    @staticmethod
+    def add_product_feedback(uid, pid, review):
+        try:
+            current_time = datetime.datetime.now()
+            rows = app.db.execute("""INSERT INTO ReviewedProduct(user_id, product_id, review, time_reviewed_product)
+                                VALUES(:user_id, :product_id, :review, :time_reviewed_product)""",
+                                  user_id=uid,
+                                  product_id=pid,
+                                  review=review,
+                                  time_reviewed_product=current_time)
+            #id = rows[0][0]
+            #return FeedbackItem.get_all(id)
+            return True
+        except Exception as e:
+            print(str(e))
+            return None
