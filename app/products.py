60f1c813f3dd7bfa87b21d6b98aa82e83bd7a914
celstlaur <csv12@duke.edu>
2023-12-03 16:25:16 -0500

    products MOSTLY finished

--- app/products.py
+++ app/products.py
@@ -6,19 +6,67 @@ import datetime
 
 from .models.product import Product
 from .models.orderfact import OrderFact
+from .models.inventory import Inventory
+from .models.seller import Seller
 
 from flask import Blueprint, request
 bp = Blueprint('products', __name__)
 
 
-@bp.route('/products/page/<product>', methods = {"GET"})
+@bp.route('/cart/add/<int:product_id>/<int:seller_id>/<int:seller_quant>')
+def add_to_cart(product_id, seller_id, seller_quant):
+    if current_user.is_authenticated:
+
+        # THIS NEEDS TO BE EDITED TO ACTUALLY WORK LOL
+
+        user_id = current_user.id
+
+        quantity = request.form["k"]
+
+        if quantity > seller_quant:
+            return jsonify({"Impossible to complete order"})
+
+        return render_template('cart.html', title='Cart', current_user=current_user, cart_items=cart_items, total_cost=total_cost)
+
+        #FeedbackItem.add_product_feedback(user_id, product_id)
+
+        #return jsonify([feedback.__dict__ for feedback in feedbacks])
+        
+        #return render_template('recent_feedbacks.html', recent_feedbacks = feedbacks)
+    
+    else:
+        return jsonify({}), 404
+
+@bp.route('/products/page/<product>', methods = {"GET", "POST"})
 def getprodpage(product):
 
-    product = Product.get(product)
+    products = Product.get(product)
 
+     # determines table size
+    page = request.args.get('page', 1, type=int)
+    per_page = 4
+    offset = (page - 1) * per_page
+
+    # logic for front and back buttons
+    if request.method == 'POST':
+        if request.form['action'] == 'next':
+            page += 1
+        elif request.form['action'] == 'prev':
+            page -= 1
+
+        return redirect(url_for('productpage.productpage', page = page))
+
+
+    sellers = Inventory.get_sellers_given_product(product, per_page, offset)
+    len_sellers = Inventory.get_len_sellers_given_prod(product)
     # render the page by adding information to the index.html file
     return render_template('productpage.html',
-                           product=product)
+                           current_user=current_user,
+                            current_page = page,
+                            page_length = per_page,
+                           product=products,
+                           sellers = sellers,
+                           len_sellers = len_sellers)
 
 @bp.route('/products/findexpensive/', methods = {"GET"})
 def find_most_expensive_products():
@@ -67,13 +115,14 @@ def sort_asc():
             page += 1
         elif request.form['action'] == 'prev':
             page -= 1
-
-        return redirect(url_for('buy.buy', page = page))
+        
+        return redirect(url_for('products.sort_asc', page = page))
 
     # render the page by adding information to the index.html file
     if current_user.is_authenticated:
         purchases = OrderFact.get_paged_orders(current_user.id, page, per_page)
         return render_template('buy.html', avail_products=filtcat, 
+                               current_user=current_user,
                                             purchase_history=purchases, 
                                             current_page = page,
                                             page_length = per_page,
@@ -83,6 +132,7 @@ def sort_asc():
                                             price = "asc")
     else:
         return render_template('buy.html', avail_products=filtcat,
+                               current_user=current_user,
                                current_page = page,
                                page_length = per_page,
                                total_avail = len,
@@ -108,12 +158,13 @@ def sort_desc():
         elif request.form['action'] == 'prev':
             page -= 1
 
-        return redirect(url_for('buy.buy', page = page))
+        return redirect(url_for('products.sort_desc', page = page))
 
     # render the page by adding information to the index.html file
     if current_user.is_authenticated:
         purchases = OrderFact.get_paged_orders(current_user.id, page, per_page)
         return render_template('buy.html', avail_products=desc, 
+                               current_user=current_user,
                                             purchase_history=purchases, 
                                             current_page = page,
                                             page_length = per_page,
@@ -123,6 +174,7 @@ def sort_desc():
                                             price = "desc")
     else:
         return render_template('buy.html', avail_products=desc,
+                               current_user=current_user,
                                current_page = page,
                                page_length = per_page,
                                total_avail = len,
@@ -151,12 +203,13 @@ def get_leq():
         elif request.form['action'] == 'prev':
             page -= 1
 
-        return redirect(url_for('buy.buy', page = page))
+        return redirect(url_for('products.get_leq', page = page))
 
     # render the page by adding information to the index.html file
     if current_user.is_authenticated:
         purchases = OrderFact.get_paged_orders(current_user.id, page, per_page)
-        return render_template('buy.html', avail_products=lenleq, 
+        return render_template('buy.html', avail_products=lenleq,
+                               current_user=current_user, 
                                             purchase_history=purchases, 
                                             current_page = page,
                                             page_length = per_page,
@@ -166,6 +219,7 @@ def get_leq():
                                             category = k)
     else:
         return render_template('buy.html', avail_products=lenleq,
+                               current_user=current_user,
                                current_page = page,
                                page_length = per_page,
                                total_avail = lenq,
@@ -194,12 +248,13 @@ def get_geq():
         elif request.form['action'] == 'prev':
             page -= 1
 
-        return redirect(url_for('buy.buy', page = page))
+        return redirect(url_for('products.get_geq', page = page))
 
     # render the page by adding information to the index.html file
     if current_user.is_authenticated:
         purchases = OrderFact.get_paged_orders(current_user.id, page, per_page)
-        return render_template('buy.html', avail_products=geq, 
+        return render_template('buy.html', avail_products=geq,
+                               current_user=current_user, 
                                             purchase_history=purchases, 
                                             current_page = page,
                                             page_length = per_page,
@@ -209,6 +264,7 @@ def get_geq():
                                             category = k)
     else:
         return render_template('buy.html', avail_products=geq,
+                               current_user=current_user,
                                current_page = page,
                                page_length = per_page,
                                total_avail = lengeq,
@@ -217,6 +273,7 @@ def get_geq():
 
 @bp.route('/products/filtercategory/', methods = {"GET", "POST"})
 def find_category():
+
 
     k = request.args.get('k')
     # get products for sale in category:
@@ -238,12 +295,13 @@ def find_category():
         elif request.form['action'] == 'prev':
             page -= 1
 
-        return redirect(url_for('buy.buy', page = page))
+        return redirect(url_for('products.find_category', page = page, k = k))
 
     # render the page by adding information to the index.html file
     if current_user.is_authenticated:
         purchases = OrderFact.get_paged_orders(current_user.id, page, per_page)
-        return render_template('buy.html', avail_products=filtcat, 
+        return render_template('buy.html', avail_products=filtcat,
+                               current_user=current_user, 
                                             purchase_history=purchases, 
                                             current_page = page,
                                             page_length = per_page,
@@ -253,6 +311,7 @@ def find_category():
                                             category = k)
     else:
         return render_template('buy.html', avail_products=filtcat,
+                               current_user=current_user,
                                current_page = page,
                                page_length = per_page,
                                total_avail = len,
@@ -261,7 +320,8 @@ def find_category():
 @bp.route('/products/filtercategory/asc/', methods = {"GET", "POST"})
 def find_category_asc():
 
-    k = request.args.get('k')
+    if not k:
+        k = request.args.get('k')
     # get products for sale in category:
 
         # determines table size
@@ -281,12 +341,13 @@ def find_category_asc():
         elif request.form['action'] == 'prev':
             page -= 1
 
-        return redirect(url_for('buy.buy', page = page))
+        return redirect(url_for('products.find_category_asc', page = page, k = k))
 
     # render the page by adding information to the index.html file
     if current_user.is_authenticated:
         purchases = OrderFact.get_paged_orders(current_user.id, page, per_page)
-        return render_template('buy.html', avail_products=filtcat, 
+        return render_template('buy.html', avail_products=filtcat,
+                               current_user=current_user, 
                                             purchase_history=purchases, 
                                             current_page = page,
                                             page_length = per_page,
@@ -297,6 +358,7 @@ def find_category_asc():
                                             price = "asc")
     else:
         return render_template('buy.html', avail_products=filtcat,
+                               current_user=current_user,
                                current_page = page,
                                page_length = per_page,
                                total_avail = len,
@@ -304,7 +366,8 @@ def find_category_asc():
                                price = "asc")
 
 
-@bp.route('/products/filterkeyword/', methods = {"GET"})
+
+@bp.route('/products/filterkeyword/', methods = {"GET", "POST"})
 def find_keyword():
 
     k = request.args.get('k')
@@ -327,12 +390,13 @@ def find_keyword():
         elif request.form['action'] == 'prev':
             page -= 1
 
-        return redirect(url_for('buy.buy', page = page))
+        return redirect(url_for('products.find_keyword', page = page, k=k))
 
     # render the page by adding information to the index.html file
     if current_user.is_authenticated:
         purchases = OrderFact.get_paged_orders(current_user.id, page, per_page)
-        return render_template('buy.html', avail_products=filtdesc, 
+        return render_template('buy.html', avail_products=filtdesc,
+                               current_user=current_user, 
                                             purchase_history=purchases, 
                                             current_page = page,
                                             page_length = per_page,
@@ -342,9 +406,135 @@ def find_keyword():
                                             keyword = k)
     else:
         return render_template('buy.html', avail_products=filtdesc,
+                               current_user=current_user,
                                current_page = page,
                                page_length = per_page,
                                total_avail = len,
                                keyword = k)
+    
+
+@bp.route('/product/edit_name/<int:product_id>', methods=['GET', 'POST'])
+def edit_name(product_id):
+    if current_user.is_authenticated:
+
+        user_id = current_user.id
+        new_text = request.form["k"]
+
+        #return render_template('submitfeedback.html')
+        
+        test = Product.edit_product_name(product_id, new_text)
+        #print(test)
+
+        #return jsonify([feedback.__dict__ for feedback in feedbacks])
+        
+        #return render_template('recent_feedbacks.html', recent_feedbacks = feedbacks)
+
+        if test is True:
+            return redirect(url_for("products.getprodpage", product = product_id))
+
+    
+    else:
+        return jsonify({}), 404
+    
+@bp.route('/product/edit_desc/<int:product_id>', methods=['GET', 'POST'])
+def edit_desc(product_id):
+    if current_user.is_authenticated:
+
+        user_id = current_user.id
+        new_text_desc = request.form["n"]
+
+        #return render_template('submitfeedback.html')
+
+        test = Product.edit_product_desc(product_id, new_text_desc)
+        #print(test)
+
+        #return jsonify([feedback.__dict__ for feedback in feedbacks])
+        
+        #return render_template('recent_feedbacks.html', recent_feedbacks = feedbacks)
+
+        if test is True:
+            return redirect(url_for("products.getprodpage", product = product_id))
+
+    
+    else:
+        return jsonify({}), 404
+    
+@bp.route('/product/edit_cat/<int:product_id>', methods=['GET', 'POST'])
+def edit_cat(product_id):
+    if current_user.is_authenticated:
+
+        user_id = current_user.id
+        new_text_cat = request.form["l"]
+
+        #return render_template('submitfeedback.html')
+
+        test = Product.edit_product_category(product_id, new_text_cat)
+        #print(test)
+
+        #return jsonify([feedback.__dict__ for feedback in feedbacks])
+        
+        #return render_template('recent_feedbacks.html', recent_feedbacks = feedbacks)
+
+        if test is True:
+            return redirect(url_for("products.getprodpage", product = product_id))
+
+    
+    else:
+        return jsonify({}), 404
+    
+@bp.route('/product/edit_price/<int:product_id>', methods=['GET', 'POST'])
+def edit_price(product_id):
+    if current_user.is_authenticated:
+
+        user_id = current_user.id
+        new_text_price = request.form["m"]
 
 
+
+        #return render_template('submitfeedback.html')
+
+        test = Product.edit_product_price(product_id, new_text_price)
+        #print(test)
+
+
+        #return jsonify([feedback.__dict__ for feedback in feedbacks])
+        
+        #return render_template('recent_feedbacks.html', recent_feedbacks = feedbacks)
+
+        if test is True:
+            return redirect(url_for("products.getprodpage", product = product_id))
+
+    
+    else:
+        return jsonify({}), 404
+    
+@bp.route('/product/create/<int:creator_id>', methods=['GET', 'POST'])
+def create(creator_id):
+    if current_user.is_authenticated:
+
+        creator_id = current_user.id
+        price = request.form["price"]
+        category = request.form["cat"]
+        name = request.form["name"]
+        new_desc = request.form['desc']
+
+
+
+        #return render_template('submitfeedback.html')
+
+        test, product_id = Product.create_new_product(name, creator_id, category, new_desc, price)
+        #print(test)
+
+        test2 = Seller.become_seller(current_user.id, current_user.email, current_user.email)
+
+
+        #return jsonify([feedback.__dict__ for feedback in feedbacks])
+        
+        #return render_template('recent_feedbacks.html', recent_feedbacks = feedbacks)
+
+        if test is True:
+            return redirect(url_for("products.getprodpage", product = product_id))
+
+    
+    else:
+        return jsonify({}), 404
