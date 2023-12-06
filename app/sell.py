4ada02ea2e3534d13a3d181408fa7f58f7f1feaa
Brennan <bh230@duke.edu>
2023-12-06 12:12:14 -0500

    added seller visualizations

--- app/sell.py
+++ app/sell.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request, current_app
+from flask import render_template, redirect, url_for, flash, request, current_app, jsonify
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
@@ -7,7 +7,7 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 from .models.user import User
 from .models.inventory import Inventory, InventoryList
-from .models.ordercontents import OrderHistory
+from .models.ordercontents import OrderHistory, OrderViz
 from . import DB
 
 import math
@@ -136,5 +136,80 @@ def new_item():
         else:
             flash('An error occurred', 'danger')
         return redirect(url_for('sell.inventory'))
+    else:
+        return redirect(url_for('index.index'))
+    
+@bp.route('/viz')
+def vis():
+    if current_user.is_authenticated:
+        # get original data in a  dictionary
+        orders = OrderViz.fetch_data(current_user.id)
+        
+        # data for visualizations, init
+        year_revenue = {}
+        state_revenue = {}
+        cat_revenue = {}
+        prod_revenue = {}
+        
+        # get all rows in orignal data
+        for o in orders:
+            
+            # different types of data to get from orders
+            year_value = o['time'].year
+            state = o['address'].split(' ')[-2]
+            cat = o['category']
+            prod = o['pname']
+            
+            # build out dictionaries for data vis
+            if year_value not in year_revenue:
+                year_revenue[year_value] = 0
+            year_revenue[year_value] += float(o['price'])
+            
+            if state not in state_revenue:
+                state_revenue[state] = 0
+            state_revenue[state] += float(o['price'])
+            
+            if cat not in cat_revenue:
+                cat_revenue[cat] = 0
+            cat_revenue[cat] += float(o['price'])
+            
+            if prod not in prod_revenue:
+                prod_revenue[prod] = 0
+            prod_revenue[prod] += float(o['price'])
+            
+        # sort relavent dictionaries to improve output 
+        sorted_year = {key: year_revenue[key] for key in sorted(year_revenue.keys())}
+        sorted_states = {key: state_revenue[key] for key in sorted(state_revenue, key=lambda k: state_revenue[k], reverse=True)}
+        sorted_prod = {key: prod_revenue[key] for key in sorted(prod_revenue, key=lambda k: prod_revenue[k], reverse=True)}
+        
+        # reduce number of slices in pie charts
+        top_5 = dict(list(sorted_states.items())[:5])
+        others_value = sum(list(sorted_states.values())[5:])
+        result_dict = {**top_5, 'others': others_value}
+        
+        top_5_prod = dict(list(sorted_prod.items())[:5])
+        others_value_prod = sum(list(sorted_prod.values())[5:])
+        result_dict_prod = {**top_5_prod, 'others': others_value_prod}
+        
+        # round output to improve output
+        for k, v in sorted_year.items():
+            sorted_year[k] = round(v, 2)
+        for k, v in result_dict.items():
+            result_dict[k] = round(v, 2)
+        for k, v in cat_revenue.items():
+            cat_revenue[k] = round(v, 2)
+        for k, v in result_dict_prod.items():
+            result_dict_prod[k] = round(v, 2)
+            
+        return render_template('viz.html', seller_check=current_user.is_seller(current_user.id), 
+                                           years = list(sorted_year.keys()), 
+                                           years_val = list(sorted_year.values()), 
+                                           states= list(result_dict.keys()), 
+                                           states_val = list(result_dict.values()),
+                                           cats = list(cat_revenue.keys()),
+                                           cats_val = list(cat_revenue.values()),
+                                           prods= list(result_dict_prod.keys()), 
+                                           prods_val = list(result_dict_prod.values()))
+    
     else:
         return redirect(url_for('index.index'))
