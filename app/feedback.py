a5c4344f079400fd38ed99753dc3f21ed4f7bb15
Sierra <sas227@duke.edu>
2023-12-08 13:07:26 -0500

    product review system up, seller review system only has delete functionality

--- app/feedback.py
+++ app/feedback.py
@@ -17,7 +17,19 @@ def recentFeedback():
 
         #return jsonify([feedback.__dict__ for feedback in feedbacks])
         
+        if feedbacks is None:
+            return jsonify({}), 404
+         
         return render_template('recent_feedbacks.html', recent_feedbacks = feedbacks)
+    
+    else:
+        return jsonify({}), 404
+    
+@bp.route('/feedback_error')
+def error():
+    if current_user.is_authenticated:
+                
+        return render_template('reviewerror.html')
     
     else:
         return jsonify({}), 404
@@ -41,9 +53,34 @@ def submit_feedback(product_id):
     else:
         return jsonify({}), 404
     
+@bp.route('/recent_feedback/edit/<int:product_id>')
+def submit_edits(product_id):
+    if current_user.is_authenticated:
+
+        user_id = current_user.id
+
+        product = Product.get(product_id)
+
+        return render_template('submitedits.html', product=product)
+
+        #FeedbackItem.add_product_feedback(user_id, product_id)
+
+        #return jsonify([feedback.__dict__ for feedback in feedbacks])
+        
+        #return render_template('recent_feedbacks.html', recent_feedbacks = feedbacks)
+    
+    else:
+        return jsonify({}), 404
+    
 @bp.route('/recent_feedback/add_review/<int:product_id>', methods=['GET', 'POST'])
 def add_feedback(product_id):
     if current_user.is_authenticated:
+        
+        # RETURN ERROR IF REVIEW HAS ALREADY BEEN MADE
+        current_reviews = FeedbackItem.get_product_reviews(current_user.id)
+        if current_reviews is not None:
+            return redirect(url_for('recent_feedback.error'))
+    
 
         user_id = current_user.id
         review_text = request.form["review"]
@@ -51,6 +88,50 @@ def add_feedback(product_id):
         #return render_template('submitfeedback.html')
 
         test = FeedbackItem.add_product_feedback(user_id, product_id, review_text)
+
+        #return jsonify([feedback.__dict__ for feedback in feedbacks])
+        
+        #return render_template('recent_feedbacks.html', recent_feedbacks = feedbacks)
+
+        if test is True:
+            return redirect(url_for('recent_feedback.recentFeedback'))
+    
+    else:
+        return jsonify({}), 404
+
+@bp.route('/recent_feedback/edit_review/<int:product_id>', methods=['GET', 'POST'])
+def edit_feedback(product_id):
+    if current_user.is_authenticated:
+        user_id = current_user.id
+        review_text = request.form["edit"]
+
+        #return render_template('submitfeedback.html')
+
+        test = FeedbackItem.edit_product_feedback(user_id, product_id, review_text)
+
+        #return jsonify([feedback.__dict__ for feedback in feedbacks])
+        
+        #return render_template('recent_feedbacks.html', recent_feedbacks = feedbacks)
+
+        if test is True:
+            return redirect(url_for('recent_feedback.recentFeedback'))
+    
+    else:
+        return jsonify({}), 404
+
+@bp.route('/recent_feedback/delete_review/<int:product_id>/<review>/<type>', methods=['POST'])
+def delete_feedback(product_id, review, type):
+    if current_user.is_authenticated:
+
+        user_id = current_user.id
+        
+
+        #return render_template('submitfeedback.html')
+
+        if type == "Product":
+            test = FeedbackItem.delete_product_feedback(user_id, product_id, review)
+        else:
+            test = FeedbackItem.delete_seller_feedback(user_id, product_id, review)
 
         #return jsonify([feedback.__dict__ for feedback in feedbacks])
         
--- app/models/feedbackitem.py
+++ app/models/feedbackitem.py
@@ -2,8 +2,10 @@ from flask import current_app as app
 import datetime
 
 class FeedbackItem:
-    def __init__(self, reviewed, review, timestamp):
+    def __init__(self, reviewed, reviewed_id, reviewed_type, review, timestamp):
         self.reviewed = reviewed
+        self.reviewed_id = reviewed_id
+        self.reviewed_type = reviewed_type
         self.review = review
         self.timestamp = timestamp
     
@@ -24,16 +26,34 @@ class FeedbackItem:
     @staticmethod
     def get_all(user_id):
         rows = app.db.execute('''
-                            (SELECT names.firstname AS reviewed, reviews.review, time_reviewed_seller AS time_reviewed
+                            (SELECT names.firstname AS reviewed, reviews.seller_id AS reviewed_id, 'Seller' AS reviewed_type, reviews.review, time_reviewed_seller AS time_reviewed
                             FROM ReviewedSeller AS reviews, Users AS names
                             WHERE reviews.user_id = :user_id
                             AND reviews.seller_id = names.id)
                             UNION
-                            (SELECT descriptions.name AS reviewed, reviews.review, time_reviewed_product AS time_reviewed
+                            (SELECT descriptions.name AS reviewed, reviews.product_id AS reviewed_id, 'Product' AS reviewed_type, reviews.review, time_reviewed_product AS time_reviewed
                             FROM ReviewedProduct AS reviews, Products AS descriptions
                             WHERE reviews.user_id = :user_id
                             AND reviews.product_id = descriptions.id)
-                            ORDER BY time_reviewed DESC LIMIT 5''', user_id = user_id)
+                            ORDER BY time_reviewed DESC''', user_id = user_id)
+        
+        return [FeedbackItem(*row) for row in rows] if rows else None
+    
+    @staticmethod
+    def get_product_reviews(user_id):
+        rows = app.db.execute('''
+                            SELECT *
+                            FROM ReviewedProduct
+                            WHERE user_id = :user_id''', user_id = user_id)
+        
+        return [FeedbackItem(*row) for row in rows] if rows else None
+    
+    @staticmethod
+    def get_seller_reviews(user_id):
+        rows = app.db.execute('''
+                            SELECT *
+                            FROM ReviewedSeller
+                            WHERE user_id = :user_id''', user_id = user_id)
         
         return [FeedbackItem(*row) for row in rows] if rows else None
     
@@ -53,3 +73,49 @@ class FeedbackItem:
         except Exception as e:
             print(str(e))
             return None
+
+    @staticmethod
+    def delete_product_feedback(uid, pid, review):
+        try:
+            rows = app.db.execute("""DELETE FROM ReviewedProduct
+                                WHERE user_id = :uid AND product_id = :pid AND review = :review""",
+                                  uid=uid,
+                                  pid=pid,
+                                  review=review)
+            #id = rows[0][0]
+            #return FeedbackItem.get_all(id)
+            return True
+        except Exception as e:
+            print(str(e))
+            return e
+    
+    @staticmethod
+    def delete_seller_feedback(uid, sid, review):
+        try:
+            rows = app.db.execute("""DELETE FROM ReviewedSeller
+                                WHERE user_id = :uid AND seller_id = :sid AND review = :review""",
+                                  uid=uid,
+                                  sid=sid,
+                                  review=review)
+            #id = rows[0][0]
+            #return FeedbackItem.get_all(id)
+            return True
+        except Exception as e:
+            print(str(e))
+            return e
+        
+    @staticmethod
+    def edit_product_feedback(uid, pid, newReview):
+        try:
+            rows = app.db.execute("""UPDATE ReviewedProduct
+                                SET review = :newReview
+                                WHERE user_id = :uid AND product_id = :pid""",
+                                  uid=uid,
+                                  pid=pid,
+                                  newReview=newReview)
+            #id = rows[0][0]
+            #return FeedbackItem.get_all(id)
+            return True
+        except Exception as e:
+            print(str(e))
+            return e
