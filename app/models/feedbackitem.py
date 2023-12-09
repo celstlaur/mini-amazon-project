579e5a6e5826bb9070434496440c9fe94e1ebcf6
Sierra <sas227@duke.edu>
2023-12-08 20:42:45 -0500

    adding stars...

--- app/models/feedbackitem.py
+++ app/models/feedbackitem.py
@@ -2,11 +2,12 @@ from flask import current_app as app
 import datetime
 
 class FeedbackItem:
-    def __init__(self, reviewed, reviewed_id, reviewed_type, review, timestamp):
+    def __init__(self, reviewed, reviewed_id, reviewed_type, review, stars, timestamp):
         self.reviewed = reviewed
         self.reviewed_id = reviewed_id
         self.reviewed_type = reviewed_type
         self.review = review
+        self.stars = stars
         self.timestamp = timestamp
     
     #@staticmethod
@@ -26,12 +27,12 @@ class FeedbackItem:
     @staticmethod
     def get_all(user_id):
         rows = app.db.execute('''
-                            (SELECT names.firstname AS reviewed, reviews.seller_id AS reviewed_id, 'Seller' AS reviewed_type, reviews.review, time_reviewed_seller AS time_reviewed
+                            (SELECT names.firstname AS reviewed, reviews.seller_id AS reviewed_id, 'Seller' AS reviewed_type, reviews.review, reviews.stars, time_reviewed_seller AS time_reviewed
                             FROM ReviewedSeller AS reviews, Users AS names
                             WHERE reviews.user_id = :user_id
                             AND reviews.seller_id = names.id)
                             UNION
-                            (SELECT descriptions.name AS reviewed, reviews.product_id AS reviewed_id, 'Product' AS reviewed_type, reviews.review, time_reviewed_product AS time_reviewed
+                            (SELECT descriptions.name AS reviewed, reviews.product_id AS reviewed_id, 'Product' AS reviewed_type, reviews.review, reviews.stars, time_reviewed_product AS time_reviewed
                             FROM ReviewedProduct AS reviews, Products AS descriptions
                             WHERE reviews.user_id = :user_id
                             AND reviews.product_id = descriptions.id)
@@ -42,7 +43,7 @@ class FeedbackItem:
     @staticmethod
     def get_product_reviews(user_id, product_id):
         rows = app.db.execute('''
-                            SELECT descriptions.name AS reviewed, reviews.product_id AS reviewed_id, 'Product' AS reviewed_type, reviews.review, time_reviewed_product AS time_reviewed
+                            SELECT descriptions.name AS reviewed, reviews.product_id AS reviewed_id, 'Product' AS reviewed_type, reviews.review, reviews.stars, time_reviewed_product AS time_reviewed
                             FROM ReviewedProduct AS reviews, Products AS descriptions
                             WHERE user_id = :user_id
                             AND product_id = :product_id''', user_id = user_id, product_id = product_id)
@@ -52,7 +53,7 @@ class FeedbackItem:
     @staticmethod
     def get_seller_reviews(user_id, seller_id):
         rows = app.db.execute('''
-                            SELECT names.firstname AS reviewed, reviews.seller_id AS reviewed_id, 'Seller' AS reviewed_type, reviews.review, time_reviewed_seller AS time_reviewed
+                            SELECT names.firstname AS reviewed, reviews.seller_id AS reviewed_id, 'Seller' AS reviewed_type, reviews.review, reviews.stars, time_reviewed_seller AS time_reviewed
                             FROM ReviewedSeller AS reviews, Users AS names
                             WHERE user_id = :user_id
                             AND seller_id = :seller_id''', user_id = user_id, seller_id = seller_id)
@@ -63,11 +64,12 @@ class FeedbackItem:
     def add_product_feedback(uid, pid, review):
         try:
             current_time = datetime.datetime.now()
-            rows = app.db.execute("""INSERT INTO ReviewedProduct(user_id, product_id, review, time_reviewed_product)
-                                VALUES(:user_id, :product_id, :review, :time_reviewed_product)""",
+            rows = app.db.execute("""INSERT INTO ReviewedProduct(user_id, product_id, review, stars, time_reviewed_product)
+                                VALUES(:user_id, :product_id, :review, :stars, :time_reviewed_product)""",
                                   user_id=uid,
                                   product_id=pid,
                                   review=review,
+                                  stars=0,
                                   time_reviewed_product=current_time)
             #id = rows[0][0]
             #return FeedbackItem.get_all(id)
@@ -80,11 +82,12 @@ class FeedbackItem:
     def add_seller_feedback(uid, sid, review):
         try:
             current_time = datetime.datetime.now()
-            rows = app.db.execute("""INSERT INTO ReviewedSeller(user_id, seller_id, review, time_reviewed_seller)
-                                VALUES(:user_id, :seller_id, :review, :time_reviewed_seller)""",
+            rows = app.db.execute("""INSERT INTO ReviewedSeller(user_id, seller_id, review, stars, time_reviewed_seller)
+                                VALUES(:user_id, :seller_id, :review, :stars, :time_reviewed_seller)""",
                                   user_id=uid,
                                   seller_id=sid,
                                   review=review,
+                                  stars=0,
                                   time_reviewed_seller=current_time)
             #id = rows[0][0]
             #return FeedbackItem.get_all(id)
